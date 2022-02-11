46c1bc7fcf8261fa86150f079384b2d71605f9ee
Kirby Ellingson <hkellingson@gmail.com>
2022-02-11 15:58:41 -0600

    foursquare got a buisness to show up finally :D

--- dis.js
+++ dis.js
@@ -2,12 +2,16 @@
 // business array
 // marker object 
 // current location object? array? DONE
-const myMap = {
-coordinates: [],
-// this gives map access to coordinates and makes the map where the user is reporting their location
-    buildMap(){
-    var map = L.map('map', {
-center: myMap.coordinates,
+let userCoords = []
+let businesses = []
+let map = {} 
+// business[]. geocodes main latitude/longitude
+// add business markers
+
+//makes the map where the user is reporting their location
+function buildMap(){
+map = L.map('map', {
+center: userCoords,
 zoom: 15,
 })
 // add tiles to map
@@ -16,13 +20,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: '1',
 }).addTo(map)
 // add marker here
-var youAreHere = L.marker(myMap.coordinates).addTo(map) // IT WORKED FIRST TIME :D
+var youAreHere = L.marker(userCoords).addTo(map) // IT WORKED FIRST TIME :D
 youAreHere.bindPopup('You Are Here !!').openPopup();
 setTimeout(() => { // this closes the popup after 5 seconds
     youAreHere.bindPopup('You Are Here !!').closePopup();
 }, 5000);
+
 }
-}
+
+
+
+
 
 async function getLocation(){
     pos = await new Promise((resolve, reject) => {
@@ -32,17 +40,32 @@ async function getLocation(){
     // this seems redundant add get rid of lat and lon variables and put them in the return value.
     return [pos.coords.latitude, pos.coords.longitude]
 }
-
-
-
-// this makes all functions run one at atime after the important function of get location runs. I think.
-window.onload = async () =>{
-    // this makes getLocation output just the coordinates in an array.
-coords = await getLocation()
-myMap.coordinates = coords
- // [0]lat,[1]lon
-myMap.buildMap()
-
+// four square buisness stuff goes here.
+async function getBusiness(business){ // returns placeInfo for business array
+    const options = {
+        method: 'GET',
+        headers: {
+        Accept: 'application/json',
+        Authorization: 'fsq3A1UBCz3prWomNfth/EYCieMPJ9xsdOJYDiJVquYVd3E='
+        }
+    };
+    
+    let lat = userCoords[0]
+    let lon = userCoords[1]
+    let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=5&ll=${lat}%2C${lon}`, options) // not sure how I was supposed to find this information myself
+    let placeInfo = await response.json()
+    return placeInfo.results
 }
+// this makes all functions run one at atime after the important function of get location runs. I think.
+window.onload = async () =>{ // this acts as a wrapper function to order the flow events running on the page.
+    // this makes getLocation output just the coordinates in an array.
+userCoords = await getLocation()
+ // [0]lat,[1]lon
+buildMap() // builds the map and gives buildMap access to coords i think.
+let business= 'restaurant'
+businesses = await getBusiness(business)
+let b = L.marker([businesses[2].geocodes.main.latitude,businesses[2].geocodes.main.longitude]).addTo(map)
+b.bindPopup(`${businesses[2].name}`).openPopup();
 
-
+// populate business from submit button
+}
